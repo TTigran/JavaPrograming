7bf9d8021dd0b8e0f26b3fd0f0178f881e6a6c1c
Tigran Martirosyan <tigran11@aod.am>
2020-01-27 22:38:56 +0400

    create fundementals module   and  writed one task

---
+++ java-fundamentals-practical/src/am/javaprogram/fundamentals/greetings/GreetingsAppRunner.java
@@ -0,0 +1,8 @@
+package am.javaprogram.fundamentals.greetings;
+
+public class GreetingsAppRunner {
+    public static void main(String[] args) {
+        final var greetingApp = new GreetingsApp();
+         greetingApp.run();
+    }
+}
