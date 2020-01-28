f6e0a0db56af91ec8b6ed90e1f5d0b5f0416befe
Tigran Martirosyan <tigran11@aod.am>
2020-01-28 10:53:26 +0400

    I did fix something task

---
+++ java-fundamentals-practical/src/am/javaprogram/fundamentals/greetings/GreetingAppRunner.java
@@ -0,0 +1,8 @@
+package am.javaprogram.fundamentals.greetings;
+
+public class GreetingAppRunner {
+    public static void main(String[] args) {
+        final var greetingApp = new GreetingApp();
+        greetingApp.run();
+    }
+}
--- java-fundamentals-practical/src/am/javaprogram/fundamentals/greetings/GreetingsAppRunner.java
+++
@@ -1,8 +0,0 @@
-package am.javaprogram.fundamentals.greetings;
-
-public class GreetingsAppRunner {
-    public static void main(String[] args) {
-        final var greetingApp = new GreetingsApp();
-         greetingApp.run();
-    }
-}
