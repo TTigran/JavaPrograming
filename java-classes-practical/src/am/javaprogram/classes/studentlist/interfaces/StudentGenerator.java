609d616bb9476c6798a807615f2b158357267924
Tigran Martirosyan <tigran11@aod.am>
2020-03-03 16:46:34 +0400

    delete some failes

--- java-classes-practical/src/am/javaprogram/classes/studentlist/interfaces/StudentGenerator.java
+++
@@ -1,7 +0,0 @@
-package am.javaprogram.classes.studentlist.interfaces;
-
-import am.javaprogram.classes.studentlist.models.Student;
-
-public interface StudentGenerator {
-    Student genarateStudent();
-}
