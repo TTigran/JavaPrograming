3052b42d6c6b2e12402d655e5c6b103897de43c1
tigran <martirosyantigran.developer@gmail.com>
2020-09-14 13:39:17 +0400

    Add loop exercises

---
+++ java-training/src/am/javaprogram/loops/GeometricForm.java
@@ -0,0 +1,101 @@
+package am.javaprogram.loops;
+
+public class GeometricForm {
+    public static void main(String[] args) {
+        int size = 5;
+
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------");
+
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j < size - i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 2 * (size - (i + 1) + 1); j > 1; j--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("------------------------------------");
+
+        for (int i = 0; i < size + 4; i++) {
+            if (i < size) {
+                for (int j = 0; j < size - i - 1; j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int j = 0; j < i - size + 1; j++) {
+                    System.out.print("  ");
+                }
+
+                for (int j = 2 * (size - ((i - size + 1) + 1) + 1); j > 1; j--) {
+                    System.out.print("* ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
